1a163d13285ce65d36e2950606325ee0b92ba341
Daniel Chui <dchui@coleburn.local>
2018-10-01 01:49:51 -0600

    update settings.py

--- ci_tutorial/settings.py
+++ ci_tutorial/settings.py
@@ -113,8 +113,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+ALLOWED_HOSTS = ["162.246.157.230"]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
